3d092ded9a2470f4f700bfb1fc6774d8288c87c8
zhufree <zhufree2013@gmail.com>
2018-02-03 19:59:35 +0800

    init @2018.2.3

---
+++ MasterGTD/app/src/main/java/info/zhufree/mastergtd/MasterApplication.kt
@@ -0,0 +1,14 @@
+package info.zhufree.mastergtd
+
+import android.app.Application
+
+/**
+ * Created by Free on 2018/2/3.
+ *
+ */
+class MasterApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // TODO
+    }
+}
